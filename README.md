e062851853e40a40550a1da5692b11a38cf4eb9a
njir <tak@ubuntu14.04>
2015-05-25 15:13:38 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#Functional Programming in Scala
+Paul Chiusano and Rúnar Bjarnason
+***Foreword by Martin Odersky***
+
+nescafe.slack.com
+
---
+++ src/test/scala/CafeSpec.scala
@@ -0,0 +1,17 @@
+package chapter1
+import org.scalatest.{Matchers, FunSuite}
+
+/**
+ * Created by tak on 15. 5. 24.
+ */
+
+class CafeSpec extends FunSuite with Matchers {
+  test("buy coffee") {
+    val cafe = new Cafe3
+    val card = new CreditCard
+
+    val (coffees, charge) = cafe.buyCoffees(card, 10)
+    assert(coffees.length == 10)
+    assert(charge.amount == 300)
+  }
+}
